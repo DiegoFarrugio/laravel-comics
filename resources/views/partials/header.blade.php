23ef3863e2276749c9141955f4a697e019693969
DiegoFarrugio <diegofarrugio0@gmail.com>
2024-02-24 16:45:58 +0100

    Completato Header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,39 +1,77 @@
-@php
-    $links = [
-        [
-            'url' => '/',
-            'label' => 'Home',
-            'active' => true,
-        ],
-        [
-            'url' => '/chi-siamo',
-            'label' => 'Chi siamo',
-            'active' => true,
-        ],
-        [
-            'url' => '/contatti',
-            'label' => 'Contatti',
-            'active' => false,
-        ],
-    ];
+@php 
+
+$HeaderNav = [
+    [
+        'url' => '/',
+        'label' => 'CHARACTERS',
+    ],
+    [
+        'url' => '/',
+        'label' => 'COMICS',
+    ],
+    [
+        'url' => '/',
+        'label' => 'MOVIES',
+    ],
+    [
+        'url' => '/',
+        'label' => 'TV',
+    ],
+    [
+        'url' => '/',
+        'label' => 'GAMES',
+    ],
+    [
+        'url' => '/',
+        'label' => 'COLLECTIBLES',
+    ],
+    [
+        'url' => '/',
+        'label' => 'VIDEOS',
+    ],
+    [
+        'url' => '/',
+        'label' => 'FANS',
+    ],
+    [
+        'url' => '/',
+        'label' => 'NEWS',
+    ],
+    [
+        'url' => '/',
+        'label' => 'SHOP',
+    ],
+];
+
+
+
 @endphp
 
 <header>
-    <nav>
-        <ul>
-            @foreach ($links as $link)
-                <li>
-                    @if ($link['active'])
-                        <a href="{{ $link['url'] }}">
-                            {{ $link['label'] }}
-                        </a>
-                    @else
-                        <del>
-                            {{ $link['label'] }}
-                        </del>
-                    @endif
-                </li>
-            @endforeach
-        </ul>
-    </nav>
+    <div class="container-header">
+        <div class="row d-flex line-height-header">
+            <div class="col-1">
+                <div>
+                    <img class="logo-header" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+                </div>
+            </div>
+            <div class="col-10">
+                <ul class="d-flex justify-content-center list-unstyled">
+                    @foreach($HeaderNav as $HeaderNavs)
+                    <li>
+                        <a class="text-decoration-none text-black" href="">{{$HeaderNavs['label']}}</a>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+            <div class="col-1">
+                <div>
+                    <input type="text" placeholder="Search..."> 
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </header>
+
